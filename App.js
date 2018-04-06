ea7141df80e4e079c55a11fcc4e8c39a9cfa6f41
Aimable <niyoai@yahoo.fr>
2018-04-06 10:42:23 +0100

    Animation Done

--- App.js
+++ App.js
@@ -18,48 +18,63 @@ const DATA = [
 
 class App extends React.Component {
 
-  renderCard(item){
-    return(
-        <Card
-            key={item.id}
-            title={item.text}
-            image={{ uri: item.uri}}
+    renderCard(item){
+        return(
+            <Card
+                key={item.id}
+                title={item.text}
+                image={{ uri: item.uri}}
 
-        >
-            <Text style={{ marginBottom: 10 }}>
-                  incididunt ut labore et dolore magna aliqua.
-            </Text>
+            >
+                <Text style={{ marginBottom: 10 }}>
+                    incididunt ut labore et dolore magna aliqua.
+                </Text>
 
-            <Button
-                icon={{ name: 'code'}}
-                backgroundColor="#03A9F4"
-                title="View Now!"
-            />
+                <Button
+                    icon={{ name: 'code'}}
+                    backgroundColor="#03A9F4"
+                    title="View Now!"
+                />
 
-        </Card>
-    );
-  }
+            </Card>
+        );
+    }
+
+    renderNoMoreCards(){
+        return(
+            <Card title="All Done!">
+                <Text style={{marginBottom: 10}}>
+                    There's no more content here!.
+                </Text>
+                <Button
+                    title="Get more!"
+                    backgroundColor="#03A9F4"
+                />
+            </Card>
+        );
+    }
 
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Deck
-            data={DATA}
-            renderCard={this.renderCard}
+    render() {
+        return (
+            <View style={styles.container}>
+                <Deck
+                    data={DATA}
+                    renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
 
-        />
-      </View>
-    );
-  }
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
 
-  },
+    },
 });
 
 export default App;
